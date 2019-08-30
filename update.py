189bdb4b36173e200712ae0ef7b7560a7d6ca642
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-08-30 08:40:56 +0100

    Update update.py

--- update.py
+++ update.py
@@ -4,5 +4,5 @@ print('you have to have git hub terminal installed for this update to work')
 time.sleep(10)
 os.system('cd ..')
 os.system('rmdir /q /s DirCleaner')
-os.system('git clone https://github.com/WHYSOEASY/DirCleaner.git
+os.system('git clone https://github.com/WHYSOEASY/DirCleaner.git')
 print('Update Finished')
