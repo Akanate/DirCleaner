ca92d834ad557e2c3a44586665c6617cf404a165
Ed Luff <beartechtalks@gmail.com>
2019-09-01 21:05:24 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,10 +1,9 @@
 import os
 import time
 import shutil
-print('you have to have git hub terminal installed for this update to work')
+print('Note: Git must be installed for the update to work.')
 time.sleep(10)
 os.chdir('..')
 os.system('rmdir /q /s DirCleaner')
 os.system('git clone https://github.com/WHYSOEASY/DirCleaner.git')
-print('Update Finished!!!!')
-
+print('Update finished.')
