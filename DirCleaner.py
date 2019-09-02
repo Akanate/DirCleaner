d26bbe53f6f3c6e364a812809080669bd0b01ceb
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-09-02 10:59:40 +0100

    Emergency changes
    When RexoGamer commited the most recent changes he forgot a colon and indentation in the If statement In admin_check() so I’ve fixed that.

--- DirCleaner.py
+++ DirCleaner.py
@@ -13,13 +13,13 @@ checkadmin = config.get("MAIN", "checkadmin")
 
 # Checks if the user is an admin.
 def admin_check():
-  if checkadmin = True
-    print('Checking this program is not running as admin...')
-    try:
-        is_admin = os.getuid() == 0
-    except AttributeError:
-        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-        if is_admin == True:
+  if checkadmin == True:
+      print('Checking this program is not running as admin...')
+      try:
+         is_admin = os.getuid() == 0
+      except AttributeError:
+         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+         if is_admin == True:
             print('You need to run this program without admin privileges. This is to prevent data loss.')
             exit()
         else:
